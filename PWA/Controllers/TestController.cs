101f26283ed51ada56ea8d7397c45c0e08c175ab
Weiyi Lai <joy777park@gmail.com>
2018-04-02 00:33:17 +0800

    加入專案檔案。

---
+++ PWA/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PWA.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ajax()
+        {
+            return Json(new { isSuccess = true }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ PWA/service-worker.js
@@ -0,0 +1,59 @@
+﻿var dataCacheName = 'PWA-learningData-v2';
+var cacheName = 'PWA-learning-v5';
+var filesToCache = [
+    '/Test/Index',
+    '/scripts/app.js',
+    '/styles/main.css',
+    '/images/icons/icon-128x128.png',
+    '/images/icons/icon-144x144.png',
+    '/images/icons/icon-152x152.png',
+    '/images/icons/icon-192x192.png',
+    '/images/icons/icon-256x256.png'
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    var dataUrl = 'https://localhost:44367/Test/Ajax';
+    if (e.request.url.indexOf(dataUrl) === 0) {
+        e.respondWith(
+            fetch(e.request)
+            .then(function (response) {
+                return caches.open(dataCacheName).then(function (cache) {
+                    cache.put(e.request.url, response.clone());
+                    console.log('[ServiceWorker] Fetched&Cached Data');
+                    return response;
+                });
+            })
+        );
+    } else {
+        e.respondWith(
+            caches.match(e.request).then(function (response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
+});
